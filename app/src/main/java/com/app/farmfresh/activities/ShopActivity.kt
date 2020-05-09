b76442a1335ac18531e94e217b3012947f3dd271
Abhirup <Ghosh>
2020-05-09 19:37:19 +0530

    added flavouring

--- app/src/main/java/com/app/farmfresh/activities/ShopActivity.kt
+++ app/src/main/java/com/app/farmfresh/activities/ShopActivity.kt
@@ -2,18 +2,40 @@ package com.app.farmfresh.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.app.farmfresh.R
+import com.google.firebase.FirebaseApp
 
 class ShopActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
-
         navController = findNavController(R.id.shop_nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        FirebaseApp.initializeApp(this)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home
+            -> {
+                navController.navigateUp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
